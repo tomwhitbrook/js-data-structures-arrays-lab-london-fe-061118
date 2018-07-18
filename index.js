b3c8021599a4fe01637953ec4797c7c7fa0261ba
Tom Whitbrook <tomwhitbrook@gmail.com>
2018-07-18 17:42:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(name) {
   drivers.push(name)
 }
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name)
+}
